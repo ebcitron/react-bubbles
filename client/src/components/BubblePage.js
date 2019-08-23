39f286b9f84ed9a6c6126bb34db2886f665a585d
Eli Citron <ebcitron@gmail.com>
2019-08-23 11:40:19 -0500

    Colors Rendering

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axiosWithAuth from '../utils/axiosWithAuth';
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -9,6 +9,14 @@ const BubblePage = () => {
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
+  useEffect(() => {
+axiosWithAuth().get("http://localhost:5000/api/colors")
+.then(res => {
+  console.log('res', res);
+  setColorList(res.data);
+})
+.catch(err => console.log('Color List err', err));
+  }, [])
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -2,12 +2,14 @@ import axios from 'axios';
 
 
 const axiosWithAuth = () => {
-    const token = localStorage.getItem('userToken');
+    //const token = localStorage.getItem('userToken');
 
     return axios.create({
         headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `${token}`
+           
+            "Authorization": localStorage.getItem('token')
         },
     });
 };
+
+export default axiosWithAuth;
